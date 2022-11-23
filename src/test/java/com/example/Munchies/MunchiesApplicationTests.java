bc3731d69bf127aeaf677ef67624018ef5ddbad2
Filip Sandor <filipsandor5@gmail.com>
2022-11-23 12:43:51 +0100

    Data source configuration

---
+++ src/test/java/com/example/Munchies/MunchiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Munchies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunchiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
