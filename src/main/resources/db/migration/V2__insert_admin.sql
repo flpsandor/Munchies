2918de7612135749319668d0da0b84c9eb8d468f
Filip Sandor <filipsandor5@gmail.com>
2022-11-23 16:14:55 +0100

    Add Flyway migration

---
+++ src/main/resources/db/migration/V2__insert_admin.sql
@@ -0,0 +1,7 @@
+INSERT INTO `munchies`.`employee`
+(`employee_firstname`,
+ `employee_lastname`,
+ `employee_email`,
+ `employee_password`,
+ `employee_role`)
+VALUES ('admin', 'admin', 'admin@admin.com', 'password', 'ADMIN');
